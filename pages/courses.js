6c9cef51d0b9452aade360d6dd6a7074e4e6209a
Prabhakar <prabhakar9585@gmail.com>
2025-12-16 19:21:34 +0530

    Upgrade header, footer, indicator dropdown & courses image (no core change)

--- pages/courses.js
+++ pages/courses.js
@@ -1,12 +1,7 @@
-import Header from "../components/Header";
-
 export default function Courses() {
   return (
-    <>
-      <Header />
-      <div className="coming-soon">
-        <img src="/coming-soon.jpg" alt="Coming Soon" />
-      </div>
-    </>
+    <div className="coming-soon">
+      <img src="/coming-soon.png" alt="Coming Soon" />
+    </div>
   );
 }
